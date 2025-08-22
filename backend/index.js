208c93d97af4fa913fb5958c7cd33d4771212a63
TNTSuperMan <tnt-super-man@outlook.com>
2025-08-22 15:02:31 +0900

    APIルーターを移動

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const session = require("express-session");
 const path = require("path");
-const router = require("./app.js");
+const router = require("./routes/index.js");
 
 const app = express();
 
