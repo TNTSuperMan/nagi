9e9e5c238309e22e94078dde789f5ecfecf1e353
TNTSuperMan <tnt-super-man@outlook.com>
2025-08-30 14:44:31 +0900

    ビルドスクリプトの構成を変更

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,30 +1,26 @@
 const webpack = require("webpack");
-const frontend = require("./webpack.frontend.js");
 
-module.exports = [
-  frontend,
-  {
-    name: "backend",
-    target: "node",
-    mode: "production",
-    entry: "./backend/index.js",
-    output: {
-      path: __dirname + "/dist",
-      filename: "server.js",
-    },
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          use: "babel-loader",
-          exclude: /node_modules/,
-        },
-      ],
-    },
-    plugins: [
-      new webpack.DefinePlugin({
-        "process.env.NODE_ENV": JSON.stringify("production"),
-      }),
+module.exports = {
+  name: "backend",
+  target: "node",
+  mode: "production",
+  entry: "./backend/index.js",
+  output: {
+    path: __dirname + "/dist",
+    filename: "server.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: "babel-loader",
+        exclude: /node_modules/,
+      },
     ],
   },
-];
+  plugins: [
+    new webpack.DefinePlugin({
+      "process.env.NODE_ENV": JSON.stringify("production"),
+    }),
+  ],
+};
