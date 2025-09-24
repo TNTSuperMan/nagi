9596744f20c24f8772c80eaf60f8edaef1b0a33d
TNTSuperMan <tnt-super-man@outlook.com>
2025-09-24 23:03:06 +0900

    Cookie寿命が短すぎるのでいったん2週間にしとく

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ app.use(session({
   secret: process.env.KEY,
   resave: false,
   saveUninitialized: true,
-  cookie: { maxAge: 60000 },
+  cookie: { maxAge: 1000 * 60 * 60 * 24 * 14 },
   store: new PostgresStore({
     reader: postgres.reader_sql,
     writer: postgres.writer_sql,
