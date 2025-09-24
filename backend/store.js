024fb8725b13d8b75c6c95d4605adae5d7da2139
TNTSuperMan <tnt-super-man@outlook.com>
2025-09-24 23:18:45 +0900

    storeからasyncを吹き飛ばす

--- backend/store.js
+++ backend/store.js
@@ -7,46 +7,43 @@ class PostgresStore extends session.Store {
     this.writer = options.writer;
     this.ttl = options.ttl || 86400;
   }
-  async get(sid, callback) {
-    try {
-      const res = await this.reader`SELECT sess FROM sessions WHERE sid = ${sid} AND expires > NOW()`;
-      callback(null, res[0]?.sess || null);
-    } catch (err) {
-      callback(err);
+  get(sid, callback) {
+    this.reader`
+      SELECT sess FROM sessions WHERE sid = ${sid} AND expires > NOW()
+    `.then(function (res) {
+    if (res.length) {
+      callback(null, res[0].sess);
+    } else {
+      callback(null, null);
     }
+  });
   }
-  async set(sid, sess, callback) {
-    try {
-      const expires = new Date(sess.expires ??= Date.now() + this.ttl * 1000);
-      
-      await this.writer`
-        INSERT INTO sessions (sid, sess, expires)
-        VALUES (${sid}, ${sess}, ${new Date(expires)})
-        ON CONFLICT (sid) DO UPDATE
-        SET sess = ${sess}, expires = ${expires}
-      `;
-      callback(null);
-    } catch (err) {
-      callback(err);
-    }
+  set(sid, sess, callback) {
+    const expires = new Date(sess.expires ??= Date.now() + this.ttl * 1000);
+    
+    this.writer`
+      INSERT INTO sessions (sid, sess, expires)
+      VALUES (${sid}, ${sess}, ${new Date(expires)})
+      ON CONFLICT (sid) DO UPDATE
+      SET sess = ${sess}, expires = ${expires}
+    `.then(function () {
+    callback(null);
+  });
   }
-  async destroy(sid, callback) {
-    try {
-      await this.writer`DELETE FROM sessions WHERE sid = ${sid}`;
-      callback(null);
-    } catch (err) {
-      callback(err);
-    }
+  destroy(sid, callback) {
+    this.writer`
+      DELETE FROM sessions WHERE sid = ${sid}
+    `.then(function () {
+    callback(null);
+  });
   }
-  async touch(sid, sess, callback) {
-    try {
-      const expires = new Date(sess.expires ??= Date.now() + this.ttl * 1000);
-
-      await this.writer`UPDATE sessions SET expires = ${expires} WHERE sid = ${sid}`;
-      callback(null);
-    } catch (err) {
-      callback(err);
-    }
+  touch(sid, sess, callback) {
+    const expires = new Date(sess.expires ??= Date.now() + this.ttl * 1000);
+    this.writer`
+      UPDATE sessions SET expires = ${expires} WHERE sid = ${sid}
+    `.then(function () {
+    callback(null);
+  });
   }
 }
 
